7bcf0bee129c5e5fc59fc0088e706dfe59cdda69
Ansh0562 <162989060+Ansh0562@users.noreply.github.com>
2025-02-03 03:54:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.1 MB (99.99%)

---
+++ submissions/0070-climbing-stairs/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        int a = 1;
+        int b = 0;
+        for(int i = 0; i < n; i++){
+            a = a+b;
+            b = a-b;
+        }
+        return a;
+        
+        
+    }
+
+}
