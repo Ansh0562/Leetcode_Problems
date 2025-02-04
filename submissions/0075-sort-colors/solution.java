b1d7fefe9853849a33330119778e34ed1d440d6f
Ansh0562 <162989060+Ansh0562@users.noreply.github.com>
2025-02-04 19:43:43 +0000

    Sync LeetCode submission Runtime - 2 ms (16.98%), Memory - 42.3 MB (99.96%)

---
+++ submissions/0075-sort-colors/solution.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.Collections;
+class Solution {
+    public void sortColors(int[] nums) {
+       Arrays.sort(nums);
+        
+        System.out.println(Arrays.toString(nums));    
+    }
+}
