e77bec7d06741245bf09eca6a666b1f2da791814
Ansh0562 <162989060+Ansh0562@users.noreply.github.com>
2025-03-19 17:32:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.1 MB (99.99%)

--- submissions/0070-climbing-stairs/solution.java
+++ submissions/0070-climbing-stairs/solution.java
@@ -1,14 +1,20 @@
 class Solution {
     public int climbStairs(int n) {
-        int a = 1;
-        int b = 0;
-        for(int i = 0; i < n; i++){
-            a = a+b;
-            b = a-b;
+        return ClimbStair(n);      
+    }
+     public static int ClimbStair(int n){
+        if(n<=2){
+            return n;
         }
-        return a;
-        
-        
+        int first = 1;
+        int second =1;
+        int result = 0;
+        for (int i=2 ; i<=n ;i++){
+            result = first+second;
+            first=second;
+            second = result;
+        }
+        return result;
     }
 
 }
