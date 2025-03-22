42d311ad53286b7136d316427b54805fe71a3d45
Ansh0562 <162989060+Ansh0562@users.noreply.github.com>
2025-03-22 04:21:28 +0000

    Sync LeetCode submission Runtime - 1 ms (95.44%), Memory - 42.6 MB (99.73%)

--- submissions/0118-pascals-triangle/solution.java
+++ submissions/0118-pascals-triangle/solution.java
@@ -1,40 +1,26 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> l = new ArrayList<>();
-
-        
         l.add(new ArrayList<>());
         l.get(0).add(1);
 
-
-        for (int i = 1; i < numRows; i++) {
-            List<Integer> pre = l.get(i - 1); 
-            l.add(new ArrayList<>());        
-            l.get(i).add(1);                
-
-            for (int j = 0; j < pre.size() - 1; j++) {
-                int r = pre.get(j) + pre.get(j + 1); 
-                l.get(i).add(r);
-            }
-
+        for (int i=1 ; i< numRows ; i++){
+            List<Integer> pre  = l.get(i-1);
+            l.add(new ArrayList<>());
             l.get(i).add(1);
+        
+        for (int j=0; j< pre.size()-1;j++){
+            int r = pre.get(j) + pre.get(j+1);
+            l.get(i).add(r);
+            
+
+
+
         }
+        l.get(i).add(1);
+        
 
-        return l; 
     }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of rows:");
-        int numRows = sc.nextInt();
-
-        Solution solution = new Solution();
-        List<List<Integer>> result = solution.generate(numRows);
-
-        // Print the Pascal's Triangle
-        System.out.println(result);
-    }
+    return l;
 }
-
+}
