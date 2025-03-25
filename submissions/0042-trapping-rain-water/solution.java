e6ca212e3e22c7d37649629b25bf9101ddce8c9e
Ansh0562 <162989060+Ansh0562@users.noreply.github.com>
2025-03-25 11:08:39 +0000

    Sync LeetCode submission Runtime - 1 ms (58.42%), Memory - 46.6 MB (99.92%)

--- submissions/0042-trapping-rain-water/solution.java
+++ submissions/0042-trapping-rain-water/solution.java
@@ -1,31 +1,23 @@
-
 class Solution {
     public int trap(int[] height) {
-        int size = height.length;
-        int [] a1= new int [size];
-        int [] a2 = new int [size];
-        a1[0] = height[0];
-        for (int  i = 1 ; i<size ; i++){
-            if (a1[i-1]>height[i]){
-                a1[i] = a1[i-1];
-            }
-            else{
-                a1[i]= height[i];
-            }
+        return Trapping(height);  
+        
+    }
+    public static int Trapping(int [] arr){
+        int n  = arr.length;
+        int [] left = new int[n];
+        left [0]=arr[0];
+        for (int i =1 ; i < n;i++){
+            left[i]=Math.max(left[i-1],arr[i]);
         }
-        a2[size-1] = height[size-1];
-        for (int i= size-2 ; i >= 0 ; i--){
-            if (a2[i+1] >= height[i]){
-                a2[i] = a2[i+1];
-            }
-            else{
-                a2[i] = height[i];
-            }
+        int [] right = new int[n];
+        right[n-1]=arr[n-1];
+        for (int i = n-2;i>=0;i--){
+            right[i] = Math.max(right[i+1],arr[i]);
         }
-        int sum =0;
-        int max =0;
-        for(int i =0 ; i < size ; i++){
-            sum += Math.min(a1[i],a2[i])-height[i];
+        int sum=0;
+        for (int i=0; i<n ; i++){
+            sum = sum + Math.min(left[i],right[i])-arr[i];
         }
         return sum;
     }
