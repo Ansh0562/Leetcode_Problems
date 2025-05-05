f85983ca55a9a030622f878664e76b2ea285fb9f
Ansh0562 <162989060+Ansh0562@users.noreply.github.com>
2025-05-05 05:33:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.6 MB (99.95%)

---
+++ submissions/0061-rotate-list/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) return head;
+
+       
+        ListNode tail = head;
+        int len = 1;
+        while (tail.next != null) {
+            len++;
+            tail = tail.next;
+        }
+
+     
+        tail.next = head;
+
+        k = k % len;
+        int stepsToNewTail = len - k;
+        ListNode newTail = head;
+        for (int i = 1; i < stepsToNewTail; i++) {
+            newTail = newTail.next;
+        }
+
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
+}
+
