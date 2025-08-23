3000161aab85e4edc68971e2abffb2e00454200a
Ansh0562 <162989060+Ansh0562@users.noreply.github.com>
2025-08-23 11:53:50 +0000

    Sync LeetCode submission Runtime - 1 ms (85.90%), Memory - 41 MB (99.98%)

--- submissions/1635-number-of-good-pairs/solution.java
+++ submissions/1635-number-of-good-pairs/solution.java
@@ -1,18 +1,13 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
-        int size = nums.length;
-        ArrayList<Integer>a1=new ArrayList<>();
         int count =0;
-        for (int i=0 ; i<size;i++){
-            for (int j=i+1;j<size;j++)
+        for (int i=0;i<nums.length;i++){
+            for (int j=i+1;j<nums.length;j++){
                 if(nums[i]==nums[j]){
                     count++;
-
                 }
-
+            }
         }
         return count;
-    
-        
     }
 }
