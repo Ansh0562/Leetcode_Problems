765a41a7408122b03771577a4fa3beb63e9043cb
Ansh0562 <162989060+Ansh0562@users.noreply.github.com>
2025-08-23 10:56:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.7 MB (99.73%)

--- submissions/1580-shuffle-the-array/solution.java
+++ submissions/1580-shuffle-the-array/solution.java
@@ -1,24 +1,19 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-       int a=0;
-       int size = (nums.length)/2;
-        int num1[]=new int[size];
-        int num2[]= new int[size];
-        int num3[] = new int[nums.length];
-        for (int i=0;i<size;i++) {
-            num1[i] = nums[i];
-            num2[i] = nums[i+size];
-
+        int l=0;
+        int r =n;
+        int arr[] = new int[2*n];
+        for(int i=0;i<nums.length;i++){
+            if(i%2==0){
+                arr[i]=nums[l];
+                l++;
+            }
+            else{
+                arr[i]=nums[r];
+                r++;
+            }
         }
-        for (int j=0;j<size;j++){
-            num3[a]=num1[j];
-            a++;
-            num3[a]=num2[j];
-            a++;
-        }
-
-        return num3;
-
+        return arr;
+        
     }
 }
-    
