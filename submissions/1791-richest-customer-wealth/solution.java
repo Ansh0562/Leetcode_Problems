0fef216695eabd6f4fe03442a26ac81af295131c
Ansh0562 <162989060+Ansh0562@users.noreply.github.com>
2025-08-24 12:48:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43 MB (99.98%)

--- submissions/1791-richest-customer-wealth/solution.java
+++ submissions/1791-richest-customer-wealth/solution.java
@@ -1,19 +1,21 @@
 import java.util.*;
 class Solution {
     public int maximumWealth(int[][] accounts) {
+        int ans  = Integer.MIN_VALUE;
        int n = accounts.length; 
-       int ans[] = new int[n];
+        
        for (int row=0;row<n;row++){
+        
         int sum=0;
         for (int col = 0 ; col<accounts[row].length;col++){
             sum += accounts[row][col];
 
         }
-        ans[row]=sum;
-
+        if(sum > ans){
+        ans = sum;
        }
-       Arrays.sort(ans);
-       int max = ans[ans.length-1];
-       return max; 
+       }
+
+       return ans;
     }
 }
