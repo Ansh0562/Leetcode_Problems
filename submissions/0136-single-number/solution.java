ba2e26f3557e3a8f4a74780c0edf5019ac484e29
Ansh0562 <162989060+Ansh0562@users.noreply.github.com>
2026-05-27 11:44:53 +0000

    Sync LeetCode submission Runtime - 1 ms (99.94%), Memory - 46.4 MB (97.92%)

--- submissions/0136-single-number/solution.java
+++ submissions/0136-single-number/solution.java
@@ -1,9 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
-    int result =0;
-      for(int num:nums){
-        result^=num;
-      }  
-      return result;    
+
+        int ans = 0;
+
+        for(int num : nums){
+            ans = ans ^ num;
+        }
+
+        return ans;
     }
 }
