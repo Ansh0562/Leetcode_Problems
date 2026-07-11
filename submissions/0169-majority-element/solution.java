c5490813a326109acb83bf7c026e7c89b1f52808
Ansh0562 <162989060+Ansh0562@users.noreply.github.com>
2026-07-11 17:46:34 +0000

    Sync LeetCode submission Runtime - 15 ms (24.54%), Memory - 52.8 MB (76.06%)

--- submissions/0169-majority-element/solution.java
+++ submissions/0169-majority-element/solution.java
@@ -1,15 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
-         int number =0,count=0;
-
-        for (int num : nums){
-            if(count==0){
-                number=num;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int maxFreq = 0;
+        int answer = 0;
+        for(int num:nums){
+            if(map.containsKey(num)){
+                int count = map.get(num);
+                map.put(num,count+1); 
             }
-            count+=(num==number)?1:-1;
-
+            else{
+        map.put(num,1);}
         }
-        return number;
-        
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        if (entry.getValue() > maxFreq) {
+        maxFreq = entry.getValue();
+        answer = entry.getKey();
+        }
+        }
+    return answer;
     }
 }
