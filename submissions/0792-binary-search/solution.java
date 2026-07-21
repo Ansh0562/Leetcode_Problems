989e76b0bb15644daf04a587016d77a2b895b49d
Ansh0562 <162989060+Ansh0562@users.noreply.github.com>
2026-07-21 17:36:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 48.1 MB (85.53%)

--- submissions/0792-binary-search/solution.java
+++ submissions/0792-binary-search/solution.java
@@ -1,22 +1,20 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0; 
-        int end  = nums.length-1;
-        int mid=0;
-        while(start<=end){
-            mid  = start+(end-start)/2;
-            if(target == nums[mid]){
-                return mid;
-            }
-            else if(target<nums[mid]){
-                end = mid-1;
-            }
-            else{
-                start = mid+1;
-            }
-            
-            
+       int n = nums.length;
+       int l=0;
+       int r = n-1;
+       while(l<=r){
+        int mid = l+(r-l)/2;
+        if(nums[mid]==target){
+            return mid;
         }
-        return -1;
+        else if(nums[mid]<target){
+            l=mid+1;
+        }
+        else{
+            r=mid-1;
+        }
+       } 
+       return -1;
     }
 }
