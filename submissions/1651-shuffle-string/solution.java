4b4a6d101f08490409ef1ded3cba7d21ae7c2257
Ansh0562 <162989060+Ansh0562@users.noreply.github.com>
2026-08-25 12:41:56 +0000

    Sync LeetCode submission Runtime - 4 ms (7.21%), Memory - 45.4 MB (72.45%)

--- submissions/1651-shuffle-string/solution.java
+++ submissions/1651-shuffle-string/solution.java
@@ -1,13 +1,10 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
-        HashMap<Integer,Character> map = new HashMap<>();
-        for(int i=0;i<indices.length;i++){
-            map.put(indices[i],s.charAt(i));
+        StringBuilder ans = new StringBuilder(s);
+
+        for(int i=0;i<s.length();i++){
+            ans.setCharAt(indices[i],s.charAt(i));
         }
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<indices.length;i++){
-            sb.append(map.get(i));
-        }
-        return sb.toString();
+        return ans.toString();
     }
 }
